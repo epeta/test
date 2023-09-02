9cfdb849cea4e12077d3b8f22c0f7625ccad9b57
epeta <143908455+epeta@users.noreply.github.com>
2023-09-02 23:23:41 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# **Objectives**
+# 
+# -list popular languages for data scientist
+# 
+# -list commonly used libraries by data scientist
+# 
+# -Show example of arithmetic expressions
+
+# Some of the popular launguages that Data scientists use are:
+# 1. Phyton
+# 2. R
+# 3. C++
+
+# Some of the commonly used libraryies used by data scientists include
+# 1. Numpy
+# 2. Pandas
+# 3. Scipy
+
+# |Data Science Tools|
+# |:----------------:|
+# |Phyton            |
+# |R                 |
+# |Tableau           |
+
+# below are a few examples of evaluating arithmetic expressions in Phyton
+
+# In[2]:
+
+
+#this simple arithmetic expressionprint 
+(3*4)+5
+
+
+# In[3]:
+
+
+#this code will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# <h2>Author
+# 
+
+# Matilda
+
+# In[ ]:
+
+
+
+
